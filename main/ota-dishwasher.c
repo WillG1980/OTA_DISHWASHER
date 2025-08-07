c63820115fee23e4f877da16b802ef15510f49aa
will@sjcnu.com <will@sjcnu.com>
2025-08-07 19:27:10 -0400

    Auto-commit on build: Thu 08/07/2025 19:27:10.74

--- main/ota-dishwasher.c
+++ main/ota-dishwasher.c
@@ -9,14 +9,6 @@
 #include "esp_https_ota.h"
 #include "esp_err.h"
 #include "esp_crt_bundle.h"
-#include <string.h>
-#include "freertos/FreeRTOS.h"
-#include "freertos/event_groups.h"
-#include "esp_wifi.h"
-#include "esp_event.h"
-#include "esp_log.h"
-#include "nvs_flash.h"
-#include "esp_netif.h"
 #include "driver/gpio.h"
 
 #include "dishwasher_programs.h"
@@ -25,12 +17,14 @@
 
 // ---- Update with your firmware URL ----
 void app_main(void) {
+    ESP_LOGI("Tag","Test % %s",APP_VERSION,FIRMWARE_URL);
     //ESP_ERROR_CHECK(nvs_flash_init());
     //wifi_init_sta();
+        printf( "Total program count: %d",NUM_PROGRAMS);
 
     for (int i=0;i<NUM_PROGRAMS;i++){
         Program_Entry Program=Programs[i];
-        ESP_LOGI(TAG,"Program Name: %s",Program.name);
+        printf("Program Name: %s",Program.name);
     }
 
 }
