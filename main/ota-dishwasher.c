a87e194a3558fe1cd8418ea326295a8e3dd6533b
will@sjcnu.com <will@sjcnu.com>
2025-08-08 20:06:30 -0400

    Auto-commit on build: Fri 08/08/2025 20:06:30.72

--- main/ota-dishwasher.c
+++ main/ota-dishwasher.c
@@ -95,12 +95,14 @@ void run_program() {
 
     printf("Program Name: %s\n", ActiveStatus.Program);
   }
-
+int min_time, max_time;
   for (size_t l = 0; l < Program.num_lines; l++) {
     ProgramLineStruct Line = Program.lines[l];
     // Do something with Line...
     printf("\nCycle: %s / %s; Time [ Min %ld Max %ld ]", Line.name_cycle,
            Line.name_step, Line.min_time, Line.max_time);
+           min_time+=Line.min_time;
+           max_time+=Line.max_time;
   }
   while (1) {
     vTaskDelay(pdMS_TO_TICKS(60 * 60 * 1000));
